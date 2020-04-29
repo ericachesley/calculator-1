341fc04097a450e6418581977f55a0d391683fe8
Erica Chesley <erchesley@gmail.com>
2020-04-29 19:28:03 +0000

    Completed all functions

--- arithmetic.py
+++ arithmetic.py
@@ -15,22 +15,34 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
 
+    return num1 * num1
 
 def cube(num1):
     """Return the cube of the input."""
+
+    return num1 * num1 * num1
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    prod = 1
+    for i in range(int(num2)):
+        prod = prod * num1
+
+    return prod
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    return num1 % num2
